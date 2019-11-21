753bafdf69b2add1f38860d8dded53135271dea4
Arun Sunil <arun.sunil@icuro.com>
2019-11-20 16:32:11 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+const app = express();
+
+const {getHomePage} = require('./routes/index');
+
+const port = 5000;
+
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'adminPage'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+global.db = db;
+
+app.set('port', process.env.port || port); 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); 
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(fileUpload()); 
+
+app.get('/', getHomePage);
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
